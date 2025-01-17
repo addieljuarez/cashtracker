1328b1275082607067ca8802fefdf38132e1e40b
Addiel Juarez <addieljuarez@gmail.com>
2025-01-17 09:47:50 -0600

    backend - cd ..

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,6 +3,17 @@ import 'dotenv/config'
 
 
 export const db = new Sequelize(process.env.DATABASE_URL, {
+    models: [__dirname + './../models/**/*'],
+    /*
+     *con este ya no se generan las columnas automaticamente 
+     *
+    define: {
+        timestamps: false
+    },
+
+    // ese es para los logs 
+    logging: false,
+    */
     dialectOptions: {
         ssl: {
             require: false
