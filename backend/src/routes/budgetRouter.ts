5a7a3c99ffec82f2f3cff127b74870153414bcaa
Addiel Juarez <addieljuarez@gmail.com>
2025-01-19 00:03:04 -0600

    backend - add middleware validations

--- backend/src/routes/budgetRouter.ts
+++ backend/src/routes/budgetRouter.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
+import { body } from 'express-validator'
 import { BudgetController } from './../controllers/BudgetController'
 const router = Router()
 
 router.get('/', BudgetController.getAll)
-router.post('/', BudgetController.create)
+router.post('/',
+    body('name').notEmpty().withMessage('El nombre del presupuesto no puede ir vacio'),
+    BudgetController.create)
 router.get('/:id', BudgetController.getById)
 router.put('/:id', BudgetController.updateById)
 router.delete('/:id', BudgetController.deletById)
