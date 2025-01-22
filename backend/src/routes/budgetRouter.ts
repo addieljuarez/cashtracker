c1dce23ebd2dd6bd4838458c3241928e050c91b7
Addiel Juarez <addieljuarez@gmail.com>
2025-01-21 18:44:39 -0600

    backend - add routes expenses

--- backend/src/routes/budgetRouter.ts
+++ backend/src/routes/budgetRouter.ts
@@ -1,20 +1,26 @@
 import { Router } from 'express'
 import { BudgetController } from './../controllers/BudgetController'
+import ExpenseController from '../controllers/ExpenseController'
 import {validateBudgetExists, validateBudgetId, validateBudgetInput} from '../middlewares/budgets'
 const router = Router()
 
 router.get('/', BudgetController.getAll)
 
+// budgets routes
 router.param('budgetId', validateBudgetId)
 router.param('budgetId', validateBudgetExists)
 
 router.post('/', validateBudgetInput, BudgetController.create)
-
 router.get('/:budgetId', BudgetController.getById)
-
 router.put('/:budgetId', validateBudgetInput, BudgetController.updateById)
-
 router.delete('/:budgetId', BudgetController.deleteById)
+
+// expenses routes
+router.get('/:budgetId/expenses', ExpenseController.getAll)
+router.post('/:budgetId/expenses', ExpenseController.create)
+router.get('/:budgetId/expenses/:expenseId', ExpenseController.getById)
+router.put('/:budgetId/expenses/:expenseId', ExpenseController.updateById)
+router.delete('/:budgetId/expenses/:expenseId', ExpenseController.deleteById)
 
 
 export default router
