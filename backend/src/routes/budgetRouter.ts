8c61380d915cfefc2ec2b1ca8fb502384d0a8eac
Addiel Juarez <addieljuarez@gmail.com>
2025-01-21 23:02:52 -0600

    backend - add middleares to route

--- backend/src/routes/budgetRouter.ts
+++ backend/src/routes/budgetRouter.ts
@@ -2,7 +2,7 @@ import { Router } from 'express'
 import { BudgetController } from './../controllers/BudgetController'
 import ExpenseController from '../controllers/ExpenseController'
 import {validateBudgetExists, validateBudgetId, validateBudgetInput} from '../middlewares/budgets'
-import {validateExpenseInput} from '../middlewares/expense'
+import {validateExpenseExists, validateExpenseId, validateExpenseInput} from '../middlewares/expense'
 const router = Router()
 
 router.get('/', BudgetController.getAll)
@@ -10,6 +10,8 @@ router.get('/', BudgetController.getAll)
 // budgets routes
 router.param('budgetId', validateBudgetId)
 router.param('budgetId', validateBudgetExists)
+router.param('expenseId', validateExpenseId)
+router.param('expenseId', validateExpenseExists)
 
 router.post('/', validateBudgetInput, BudgetController.create)
 router.get('/:budgetId', BudgetController.getById)
@@ -17,10 +19,10 @@ router.put('/:budgetId', validateBudgetInput, BudgetController.updateById)
 router.delete('/:budgetId', BudgetController.deleteById)
 
 // expenses routes
-router.get('/:budgetId/expenses', validateExpenseInput, ExpenseController.getAll)
-router.post('/:budgetId/expenses', ExpenseController.create)
+router.get('/:budgetId/expenses', ExpenseController.getAll)
+router.post('/:budgetId/expenses',validateExpenseInput, ExpenseController.create)
 router.get('/:budgetId/expenses/:expenseId', ExpenseController.getById)
-router.put('/:budgetId/expenses/:expenseId', ExpenseController.updateById)
+router.put('/:budgetId/expenses/:expenseId', validateExpenseInput, ExpenseController.updateById)
 router.delete('/:budgetId/expenses/:expenseId', ExpenseController.deleteById)
 
 
