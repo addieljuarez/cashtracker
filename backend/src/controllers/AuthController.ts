2ef35d563606eb224735a4a48a9ac516d4442771
Addiel Juarez <addieljuarez@gmail.com>
2025-01-23 15:53:27 -0600

    backend - user save in DB

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -1,9 +1,22 @@
 import type { Request, Response } from 'express'
+import User from '../models/User'
 
 export class AuthController {
     
     static createAccount  = async (req: Request, res: Response) => {
-    
+        try{
+            const user = new User(req.body)
+            const response = await user.save()
+            res.json({
+                status: 'Cuenta creada',
+                response
+            })
+        }catch(error){
+            console.log('error en createAccount', error)
+            res.status(500).json({
+                error: 'hubo en error en createAccount'
+            })
+        }
     }
 }
 
