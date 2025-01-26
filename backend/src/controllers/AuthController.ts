d7115a328382ac48068ce85f92f65c5ec1071145
Addiel Juarez <addieljuarez@gmail.com>
2025-01-25 20:45:20 -0600

    backend - validate password in login

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -1,6 +1,6 @@
 import type { Request, Response } from 'express'
 import User from '../models/User'
-import {hashPassword} from '../utils/auth'
+import {checkPassword, hashPassword} from '../utils/auth'
 import {generateToken} from '../utils/token'
 import AuthEmail from '../emails/AuthEmail'
 
@@ -62,7 +62,7 @@ export class AuthController {
     }
 
     static login = async(req: Request, res: Response) => {
-        const { email } = req.body
+        const { email, password } = req.body
         const user = await User.findOne({
             where: {email}
         })
@@ -80,6 +80,13 @@ export class AuthController {
             })
         }
 
+        const passwordIsCheck = await checkPassword(password, user.password);
+        if(!passwordIsCheck){
+            const error = new Error('Password inconrrecto')
+            res.status(401).json({
+                error: error.message
+            })
+        }
         res.json(user)
     }
 }
