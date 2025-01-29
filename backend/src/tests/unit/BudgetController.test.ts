a1ca9dfb5fc215123e367d4cb9d7d1d31d8e5398
Addiel Juarez <addieljuarez@gmail.com>
2025-01-29 00:05:39 -0600

    backend - add foreach to multiples test

--- backend/src/tests/unit/BudgetController.test.ts
+++ backend/src/tests/unit/BudgetController.test.ts
@@ -5,6 +5,18 @@ import Budget from '../../models/Budget'
 
 
 describe('BudgetController.getAll', () => {
+
+    beforeEach(() => {
+        Budget.findAll = jest.fn().mockReset()
+
+        Budget.findAll = jest.fn().mockImplementation((options) => {
+            console.log('options: ', options.where.userId)
+            const updatedBudgets = budgets.filter(budget => budget.userId === options.where.userId)
+            // Budget.findAll = jest.fn().mockResolvedValue(updatedBudgets)
+            return Promise.resolve(updatedBudgets)
+        })
+    })
+
     it('should retrive 2 budgets with ID=1', async () => {
         expect(budgets).toHaveLength(3)
         expect(budgets).not.toHaveLength(0)
@@ -18,8 +30,8 @@ describe('BudgetController.getAll', () => {
         })
         
         const res = createResponse()
-        const updatedBudgets = budgets.filter(budget => budget.userId === req.user.id)
-        Budget.findAll = jest.fn().mockResolvedValue(updatedBudgets)
+        // const updatedBudgets = budgets.filter(budget => budget.userId === req.user.id)
+        // Budget.findAll = jest.fn().mockResolvedValue(updatedBudgets)
         await BudgetController.getAll(req, res)
 
         const data = res._getJSONData()
@@ -27,6 +39,58 @@ describe('BudgetController.getAll', () => {
         console.log('data: ===', data)
 
         expect(data).toHaveLength(2)
+        expect(res.statusCode).toBe(200)
+        expect(res.statusCode).not.toBe(404)
+
+    })
+
+    it('should retrive 1 budgets with ID=2', async () => {
+        expect(budgets).toHaveLength(3)
+        expect(budgets).not.toHaveLength(0)
+
+        const req = createRequest({
+            method: 'GET', // esto se puede ir pero sirve para documentar 
+            url: '/api/budgets', // esto se puede ir pero sirve para documentar
+            user: {
+                id: 2
+            }
+        })
+        
+        const res = createResponse()
+        // aqui iba el codigo del foreach
+        await BudgetController.getAll(req, res)
+
+        const data = res._getJSONData()
+
+        console.log('data: ===', data)
+
+        expect(data).toHaveLength(1)
+        expect(res.statusCode).toBe(200)
+        expect(res.statusCode).not.toBe(404)
+
+    })
+
+    it('should retrive 0 budgets with ID=3', async () => {
+        expect(budgets).toHaveLength(3)
+        expect(budgets).not.toHaveLength(0)
+
+        const req = createRequest({
+            method: 'GET', // esto se puede ir pero sirve para documentar 
+            url: '/api/budgets', // esto se puede ir pero sirve para documentar
+            user: {
+                id: 3
+            }
+        })
+        
+        const res = createResponse()
+         // aqui iba el codigo del foreach
+        await BudgetController.getAll(req, res)
+
+        const data = res._getJSONData()
+
+        console.log('data: ===', data)
+
+        expect(data).toHaveLength(0)
         expect(res.statusCode).toBe(200)
         expect(res.statusCode).not.toBe(404)
 
