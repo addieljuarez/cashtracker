b30dae15c347af2bba24bc82db8050abb3f77fb2
Addiel Juarez <addieljuarez@gmail.com>
2025-01-31 00:41:08 -0600

    backend - testing middleware budgets hasAccess function

--- backend/src/tests/unit/middlewares/budgets.test.ts
+++ backend/src/tests/unit/middlewares/budgets.test.ts
@@ -1,9 +1,9 @@
 import { createRequest, createResponse, RequestMethod, ResponseOptions } from 'node-mocks-http'
-import { validateBudgetExists } from '../../../middlewares/budgets'
+import { hasAcess, validateBudgetExists } from '../../../middlewares/budgets'
 import Budget from '../../../models/Budget'
 import { budgets } from '../../mocks/budgets'
 
-describe('gudget - validateBudgetId', () => {
+describe('budget - validateBudgetId', () => {
     it('should handle non exists budget', async() => {
         const req = createRequest({
             params: {
@@ -61,5 +61,45 @@ describe('gudget - validateBudgetId', () => {
         })
         expect(next).not.toHaveBeenCalled()
         expect(next).toHaveBeenCalledTimes(0)
+    })
+})
+
+
+describe('budget - hasAcess', () => {
+    it('should handle access to user', async() => {
+        const req = createRequest({
+            budget: budgets[0],
+            user: { 
+                id: 2
+            }
+        })
+        const res = createResponse()
+        const next = jest.fn()
+        
+        await hasAcess(req, res, next)
+        
+        const data = res._getJSONData()
+        expect(res.statusCode).toBe(401),
+        expect(next).not.toHaveBeenCalled()
+        expect(next).toHaveBeenCalledTimes(0),
+        expect(data).toEqual({
+            error: 'Accion no valida'
+        })
+    })
+
+    it('should handle access to user', async() => {
+        const req = createRequest({
+            budget: budgets[0],
+            user: { 
+                id: 1
+            }
+        })
+        const res = createResponse()
+        const next = jest.fn()
+        
+        await hasAcess(req, res, next)
+        
+        expect(next).toHaveBeenCalled()
+        expect(next).toHaveBeenCalledTimes(1)
     })
 })
