e2e0429447044d2fff585a2dd053c2cbe1d30caf
Addiel Juarez <addieljuarez@gmail.com>
2025-02-02 11:17:02 -0600

    backend - integration testing init

---
+++ backend/src/tests/integration/app.test.ts
@@ -0,0 +1,30 @@
+import request from 'supertest'
+import server, { connectDB } from '../../server'
+
+
+describe('TEST', () => {
+
+    beforeEach(async() => {
+        await connectDB()
+    })
+    it('should return a 200 status code from the homepage url', async () => {
+        const response = await request(server).get('/')
+        console.log('response///', response.statusCode)
+        console.log('response///', response.text)
+
+        expect(response.statusCode).toBe(200)
+        expect(response.text).toBe('API is running')
+    })
+    
+})
+
+
+describe('Authentication - Create', () => {
+    it('should displkay validation errors when form is empty', async() => {
+        const response = await request(server)
+            .post('/api/auth/create-account')
+            .send({})
+
+        console.log('response Auth: ', response.body)
+    })
+})
