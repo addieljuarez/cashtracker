e5df4c6e350547456a158d5afae6849e2523fc82
Addiel Juarez <addieljuarez@gmail.com>
2025-02-05 17:51:54 -0600

    frontend - create login and metadata page.tsx

---
+++ frontend/app/auth/login/page.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: 'CashTracker - Iniciar sesion',
+    description: 'CashTracker - Iniciar sesion'
+}
+
+export default function LoginPage(){
+    return (
+        <>
+            <h1 className='font-black text-6xl text-purple-950'>Iniciar Sesión</h1>
+            <p className="text-3xl font-bold">y controla tus <span className="text-amber-500">finanzas</span></p>
+        </>
+    )
+}
