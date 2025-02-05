0f41c6890b3e83920d49e743bff923f30bf037b8
Addiel Juarez <addieljuarez@gmail.com>
2025-02-05 13:16:29 -0600

    frontend - change font and create page register

---
+++ frontend/app/auth/register/page.tsx
@@ -0,0 +1,8 @@
+export default function RegisterPage(){
+    return(
+        <>
+            <h1 className="font-black text-6xl text-purple-950">Crea una cuenta</h1>
+            <p className="text-3xl font-bold">y controla tus <span className="text-amber-500">finanzas</span></p>
+        </>
+    )
+}
