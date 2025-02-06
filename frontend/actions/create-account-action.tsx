9231526a1b4c0dfd5a48ed4998d882a41dfd74b5
Addiel Juarez <addieljuarez@gmail.com>
2025-02-06 15:13:12 -0600

    frontend - create object register data from formdata

--- frontend/actions/create-account-action.tsx
+++ frontend/actions/create-account-action.tsx
@@ -2,4 +2,9 @@
 
 export async function register(formData: FormData){
     console.log('desde server action,formData', formData)
+    const registerData = {
+        name: formData.get('name'),
+        email: formData.get('email'),
+        passowrd: formData.get('password')
+    }
 }
