7725d6a696420de0d2ffea8170bd3dea188270ea
Addiel Juarez <addieljuarez@gmail.com>
2025-02-06 15:06:49 -0600

    frontend - create file server action create account

---
+++ frontend/actions/create-account-action.tsx
@@ -0,0 +1,5 @@
+"use server"
+
+export async function register(formData: FormData){
+    console.log('desde server action,formData', formData)
+}
--- frontend/components/auth/RegisterForm.tsx
+++ frontend/components/auth/RegisterForm.tsx
@@ -1,20 +1,27 @@
 "use client"
+
+import { register } from "@/actions/create-account-action"
+
 export default function RegisterForm(){
     return(
         <form 
             className="mt-14 space-y-5"
             noValidate
+            action={register}
         >
             <div className="flex flex-col gap-2">
                 <label
                     className="font-bold text-2xl"
-                    htmlFor="email"
-                >Email</label>
+                    htmlFor="email" 
+                >
+                    Email
+                </label>
                 <input
                     id="email"
                     type="email"
                     placeholder="Email de registro"
                     className="w-full border border-gray-300 p-3 rounded-lg"
+                    name="email"
                 />
             </div>
 
